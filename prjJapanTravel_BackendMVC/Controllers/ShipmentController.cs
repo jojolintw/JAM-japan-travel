f5f354c1ffce16c50d5571820b0c8a5614902490
莊智傑 <chaosabyss73@gmail.com>
2024-10-08 15:53:23 +0800

    加入專案檔案。

---
+++ prjJapanTravel_BackendMVC/Controllers/ShipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjJapanTravel_BackendMVC.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

0ab1741c352fca0abe8789ebd36990f66f88a60f
jojo Lin <poopoo138538@hotmail.com>
2024-10-11 16:56:56 +0800

    +edit

--- prjJapanTravel_BackendMVC/Controllers/ShipmentController.cs
+++ prjJapanTravel_BackendMVC/Controllers/ShipmentController.cs
@@ -72,6 +72,57 @@ namespace prjJapanTravel_BackendMVC.Controllers
 
             return RedirectToAction("Index"); // 刪除後返回列表頁面
         }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound(); // 返回 404 找不到頁面
+            }
+
+            // 根據 id 查詢對應的 Route
+            var route = _context.Routes.FirstOrDefault(r => r.RouteId == id);
+
+            if (route == null)
+            {
+                return NotFound(); // 如果找不到對應的 Route，返回 404
+            }
+
+            // 將出發港口和目的地港口的選項列表傳給視圖
+            ViewBag.OriginPortList = new SelectList(_context.Ports.ToList(), "PortId", "PortName", route.OriginPortId);
+            ViewBag.DestinationPortList = new SelectList(_context.Ports.ToList(), "PortId", "PortName", route.DestinationPortId);
+
+            return View(route); // 返回編輯頁面，並將 route 資料傳遞給視圖
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Models.Route updatedRoute)
+        {
+            if (id != updatedRoute.RouteId)
+            {
+                return NotFound(); // 如果路徑中的 id 和提交表單中的 RouteId 不匹配，返回 404
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // 更新資料庫中的資料
+                    _context.Update(updatedRoute);
+                    _context.SaveChanges(); // 儲存變更到資料庫
+                }
+                catch (Exception ex)
+                {
+                    // 如果發生例外，可以記錄例外或顯示錯誤信息
+                    ModelState.AddModelError("", "更新失敗: " + ex.Message);
+                    return View(updatedRoute); // 返回編輯頁面，並顯示錯誤信息
+                }
+
+                // 更新成功後，重定向回到列表頁面
+                return RedirectToAction("Index");
+            }
+
+            // 如果 ModelState 無效，則重新顯示編輯頁面
+            return View(updatedRoute);
+        }
 
 
     }

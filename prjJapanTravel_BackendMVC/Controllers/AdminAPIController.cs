948a8c581bd995efc19ade3c1ff10a6cde39923e
Jack <chaosabyss73@gmail.com>
2024-10-11 17:55:44 +0800

    1011-2

---
+++ prjJapanTravel_BackendMVC/Controllers/AdminAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjJapanTravel_BackendMVC.Models;
+
+namespace prjJapanTravel_BackendMVC.Controllers
+{
+    public class AdminAPIController : Controller
+    {
+        public JapanTravelContext _context;
+
+        public AdminAPIController(JapanTravelContext context) 
+        {
+            _context=context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetData(int adminId)
+        {
+            Admin ad = _context.Admins.FirstOrDefault(a => a.AdminId == adminId);
+            return Json(ad);
+        }
+    }
+}

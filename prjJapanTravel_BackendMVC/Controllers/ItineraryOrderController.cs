e6fd3ea42ed22cbe174f4c4ade9e901c1bb7f586
Luke <qwe58912@gmail.com>
2024-10-12 23:38:10 +0800

    20241012 1.行程List 大致OK (行程名稱待處理) 2.行程Create OK (下拉式選單待處理) 3.OrderViewModel

--- prjJapanTravel_BackendMVC/Controllers/ItineraryOrderController.cs
+++ prjJapanTravel_BackendMVC/Controllers/ItineraryOrderController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using prjJapanTravel_BackendMVC.Models;
+using prjJapanTravel_BackendMVC.ViewModels.OrderViewModels;
+using prjJapanTravel_BackendMVC.ViewModels.ProductViewModels;
 
 namespace prjJapanTravel_BackendMVC.Controllers
 {
@@ -13,13 +17,39 @@ namespace prjJapanTravel_BackendMVC.Controllers
 
         public IActionResult List()
         {
-            var datas = _context.ItineraryOrders.Select(m => m);
+            var datas = _context.ItineraryOrders
+                .Include(o => o.Member)
+                .Include(o => o.ItineraryDateSystem)
+                    //.ThenInclude(oi => oi.ItinerarySystemId)
+                .Include(o => o.PaymentMethod)
+                .Include(o => o.PaymentStatus)
+                .Include(o => o.Coupon)
+                .Select(m => new ItineraryOrderListViewModel()
+            {
+                訂單編號 = m.ItineraryOrderNumber,
+                會員 = m.Member.MemberName,
+                行程編號 = m.ItineraryDateSystemId,
+                數量 = m.Quantity,
+                下單時間 = m.OrderTime,
+                付款方式 = m.PaymentMethod.PaymentMethod1,
+                付款狀態 = m.PaymentStatus.PaymentStatus1,
+                訂單狀態 = m.OrderStatus.OrderStatus1,
+                優惠券 = m.Coupon.CouponName,
+                總金額 = m.TotalAmount
+            });
             return View(datas);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ItineraryOrder io)
+        {
+            _context.ItineraryOrders.Add(io);
+            _context.SaveChanges();
+            return RedirectToAction("List");
         }
 
         public IActionResult Edit()

3ab4e78ca623f5f913a7b398b83c7bb453cddf1e
Luke <qwe58912@gmail.com>
2024-10-15 00:18:58 +0800

    20241015 行程List、Edit

--- prjJapanTravel_BackendMVC/Controllers/ItineraryOrderController.cs
+++ prjJapanTravel_BackendMVC/Controllers/ItineraryOrderController.cs
@@ -19,8 +19,8 @@ namespace prjJapanTravel_BackendMVC.Controllers
         {
             var datas = _context.ItineraryOrders
                 .Include(o => o.Member)
-                //.Include(o => o.ItineraryDateSystem)
-                //    .ThenInclude(oi => oi.ItinerarySystemId)
+                .Include(o => o.ItineraryDateSystem)
+                    .ThenInclude(oi => oi.ItinerarySystem)
                 .Include(o => o.PaymentMethod)
                 .Include(o => o.PaymentStatus)
                 .Include(o => o.Coupon)
@@ -29,7 +29,7 @@ namespace prjJapanTravel_BackendMVC.Controllers
                 行程訂單編號 = m.ItineraryOrderId,
                 訂單編號 = m.ItineraryOrderNumber,
                 會員 = m.Member.MemberName,
-                //行程編號 = m.ItineraryDateSystemId,
+                行程名稱 = m.ItineraryDateSystem.ItinerarySystem.ItineraryName,
                 //數量 = m.Quantity,
                 下單時間 = m.OrderTime,
                 付款方式 = m.PaymentMethod.PaymentMethod1,
@@ -56,12 +56,22 @@ namespace prjJapanTravel_BackendMVC.Controllers
         public IActionResult Edit(int? id)
         {
             var data = _context.ItineraryOrders
-                .Include(io => io.Member)
-                .FirstOrDefault(io => io.ItineraryOrderId == id);
-            if (data == null)
-                return RedirectToAction("List");
-
-            //ViewBag.MemberName = ItineraryOrder
+                .Where(i => i.ItineraryOrderId == id)
+                .Select(i => new ItineraryOrderListViewModel()
+            {
+                行程訂單編號 = i.ItineraryOrderId,
+                會員 = i.Member.MemberName,
+                行程編號 = i.ItineraryDateSystemId,
+                數量 = i.Quantity,
+                下單時間 = i.OrderTime,
+                付款方式 = i.PaymentMethod.PaymentMethod1,
+                付款狀態 = i.PaymentStatus.PaymentStatus1,
+                付款時間 = i.PaymentTime,
+                訂單狀態 = i.OrderStatus.OrderStatus1,
+                優惠券 = i.Coupon.CouponName,
+                總金額 = i.TotalAmount,
+            }).FirstOrDefault();
+            
 
             return View(data);
         }
@@ -69,20 +79,29 @@ namespace prjJapanTravel_BackendMVC.Controllers
         [HttpPost]
         public IActionResult Edit(ItineraryOrderListViewModel vm)
         {
-            var data = _context.ItineraryOrders.FirstOrDefault(io => io.ItineraryOrderId == vm.行程訂單編號);
+            var data = _context.ItineraryOrders
+                .Include(io => io.Member)
+                .Include(io => io.PaymentMethod)
+                .Include(io => io.PaymentStatus)
+                .Include(io => io.OrderStatus)
+                .Include(io => io.Coupon)
+                .Include(io => io.ItineraryDateSystem)
+                    .ThenInclude(io => io.ItinerarySystem)
+                .FirstOrDefault(io => io.ItineraryOrderId == vm.行程訂單編號);
             if(data ==null)
                 return RedirectToAction("List");
             data.ItineraryOrderId = (int)vm.行程訂單編號;
-            data.ItineraryOrderNumber = vm.訂單狀態;
-            data.MemberId = (int)vm.會員編號;
-            data.ItineraryDateSystemId = (int)vm.行程編號;
+            data.ItineraryOrderNumber = vm.訂單編號;
+            data.Member.MemberName = vm.會員;
+            //data.ItineraryDateSystemId = (int)vm.行程編號;
+            data.ItineraryDateSystem.ItinerarySystem.ItineraryName = vm.行程名稱;
             data.Quantity = (int)vm.數量;
             data.OrderTime = vm.下單時間;
-            data.PaymentMethodId = (int)vm.付款方式編號;
-            data.PaymentStatusId = (int)vm.付款狀態編號;
+            data.PaymentMethod.PaymentMethod1 = vm.付款方式;
+            data.PaymentStatus.PaymentStatus1 = vm.付款狀態;
             data.PaymentTime = vm.付款時間;
-            data.OrderStatusId = (int)vm.訂單狀態編號;
-            data.CouponId = (int)vm.優惠券編號;
+            data.OrderStatus.OrderStatus1 = vm.訂單狀態;
+            data.Coupon.CouponName = vm.優惠券;
             data.TotalAmount = vm.總金額;
 
             _context.SaveChanges();

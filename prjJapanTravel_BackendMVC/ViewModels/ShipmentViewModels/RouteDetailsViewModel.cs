33dc451ab6b35c1b563a412753f4e96d2c2a7df6
jojo Lin <poopoo138538@hotmail.com>
2024-10-19 00:06:16 +0800

    Revert "temp3"
    This reverts commit 0498bbca5609d2c76b35e51feac2f5b53236da2f.

--- prjJapanTravel_BackendMVC/ViewModels/ShipmentViewModels/RouteDetailsViewModel.cs
+++ prjJapanTravel_BackendMVC/ViewModels/ShipmentViewModels/RouteDetailsViewModel.cs
@@ -1,30 +1,17 @@
-﻿namespace prjJapanTravel_BackendMVC.ViewModels
+﻿using prjJapanTravel_BackendMVC.Models;
+
+namespace prjJapanTravel_BackendMVC.ViewModels.ShipmentViewModels
 {
     public class RouteDetailsViewModel
     {
-        public int RouteId { get; set; }
-        public string OriginPortName { get; set; }
-        public string DestinationPortName { get; set; }
-        public decimal Price { get; set; }
-        public string RouteDescription { get; set; }
-        public List<ScheduleViewModel> Schedules { get; set; }
-        public List<RouteImageViewModel> RouteImages { get; set; }
-    }
+        public Models.Route Route { get; set; }
+        public IEnumerable<Schedule> Schedules { get; set; }
+        public IEnumerable<RouteImage> RouteImages { get; set; }
 
-    public class ScheduleViewModel
-    {
-        public int ScheduleId { get; set; }
-        public DateTime DepartureTime { get; set; }
-        public DateTime ArrivalTime { get; set; }
-        public int Seats { get; set; }
-        public int Capacity { get; set; }
-    }
+        // 用於新增圖片
+        public RouteImage NewRouteImage { get; set; } = new RouteImage();
 
-    public class RouteImageViewModel
-    {
-        public int RouteImageId { get; set; }
-        public string RouteImageBase64 { get; set; } // 用來存儲轉換後的Base64圖片字串
-        public string RouteImageDescription { get; set; }
+        // 用於新增排班
+        public Schedule NewSchedule { get; set; } = new Schedule();
     }
-
 }

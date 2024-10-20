c9677618ef9e94173ddd0ff8abb329b6cab2e8b2
Luke <qwe58912@gmail.com>
2024-10-20 21:07:45 +0800

    20241020
    有改跟沒改一樣，反正先推就對了
    
    問題1 進入修改畫面行程及行程日期不會自動帶入原資料，需重新選擇
    問題2 修改時未選擇行程日期會錯誤，故try catch跳回list

--- prjJapanTravel_BackendMVC/ViewModels/OrderViewModels/KeywordViewModel.cs
+++ prjJapanTravel_BackendMVC/ViewModels/OrderViewModels/KeywordViewModel.cs
@@ -3,5 +3,7 @@
     public class KeywordViewModel
     {
         public string txtKeyword { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }

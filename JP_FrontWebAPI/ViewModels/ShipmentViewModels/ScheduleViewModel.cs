24bd02e2f9c05db8b490f5e03954d09afa9a849c
jojo <User@DESKTOP-RAE7E6C>
2024-11-06 16:04:29 +0800

    1106

--- JP_FrontWebAPI/ViewModels/ShipmentViewModels/ScheduleViewModel.cs
+++ JP_FrontWebAPI/ViewModels/ShipmentViewModels/ScheduleViewModel.cs
@@ -2,4 +2,16 @@
 {
     public int ScheduleId { get; set; }
     public DateTime DepartureTime { get; set; }
+
+    public int RouteId { get; set; }
+
+
+    public DateTime ArrivalTime { get; set; }
+
+    public int Seats { get; set; }
+
+    public int Capacity { get; set; }
+
+    public virtual Route Route { get; set; }
+
 }

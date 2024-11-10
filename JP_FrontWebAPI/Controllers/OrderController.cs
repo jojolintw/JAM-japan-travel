e5ee8d78a9e397a317f54ced6002eee1c079bf5e
Luke <qwe58912@gmail.com>
2024-11-10 22:45:47 +0800

    信箱內容尚未完成

--- JP_FrontWebAPI/Controllers/OrderController.cs
+++ JP_FrontWebAPI/Controllers/OrderController.cs
@@ -32,16 +32,58 @@ namespace JP_FrontWebAPI.Controllers
         }
         
         [HttpGet("sendOrderInfoEmail")]
-        public IActionResult sendOrderInfoEmail()
+        public async Task<IActionResult> sendOrderInfoEmail([FromBody] OrderData orderData)
         {
-            string to = "qwe58912@gmail.com";
-            string subject = "test";
-            string body = "hi";
+            string orderContent = System.IO.File.ReadAllText("html/orderContent.html");
 
-            _emailService.SendEmailAsync(to, subject, body);
+            orderContent = orderContent.Replace("{{userName}}", "Luke");
+
+            string to = "qwe58912@gmail.com";
+            string subject = "Japan Activity Memory (JAM) 訂單通知";
+            //string body = GenerateHtmlEmail("Luke",orderData.cart,(decimal)orderData.totalAmount);
+            string body = "123";
+
+            await _emailService.SendEmailAsync(to, subject, body);
 
             return Ok((new {result="success"}));
         }
+
+
+        //public string GenerateHtmlEmail(string customerName, List<CartItems> CartItems, decimal totalAmount)
+        //{
+        //    if (CartItems == null || CartItems.Count == 0)
+        //    {
+        //        throw new ArgumentException("購物車沒有商品資料", nameof(CartItems));
+        //    }
+
+        //    var htmlContent = @"
+        //    <html>
+        //    <body>
+        //        <h1>訂單確認信</h1>
+        //        <p>親愛的 " + customerName + @",</p>
+        //        <p>感謝您在我們網站上購物，以下是您的訂單詳細資料：</p>
+        //        <table border='1'>
+        //            <tr><th>商品名稱</th><th>數量</th><th>單價</th></tr>";
+
+        //    foreach (var item in CartItems)
+        //    {
+        //        htmlContent += $@"
+        //        <tr>
+        //            <td>{item.name}</td>
+        //            <td>{item.quantity}</td>
+        //            <td>NT$ {item.price}</td>
+        //        </tr>";
+        //    }
+
+        //    htmlContent += $@"
+        //        </table>
+        //        <p><strong>總金額：NT$ {totalAmount}</strong></p>
+        //        <p>若有任何問題，請隨時與我們聯繫。</p>
+        //    </body>
+        //    </html>";
+
+        //    return htmlContent;
+        //}
 
         [HttpPost("CreateOrder")]
         public IActionResult CreateOrder([FromBody] OrderData orderData)

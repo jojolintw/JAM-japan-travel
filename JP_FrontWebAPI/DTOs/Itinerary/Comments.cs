bc04de780ec9788c7c69216b16fcb2c45809bb49
Ting <geniusyang924@gmail.com>
2024-11-15 20:14:03 +0800

    評論API

---
+++ JP_FrontWebAPI/DTOs/Itinerary/Comments.cs
@@ -0,0 +1,11 @@
+﻿namespace JP_FrontWebAPI.DTOs.Itinerary
+{
+    public class Comments
+    {
+        public int ItinerarySystemId { get; set; }
+        public int? CommentStar { get; set; }
+
+        public string CommentContent { get; set; }
+        public string? CommentDate { get; set; }
+    }
+}
